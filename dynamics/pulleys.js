6bbdb819c553833b6f0be8bf7dc714cea4af067e
stashito <s.ostyks@gmail.com>
2021-05-18 19:58:59 -0500

    pulleys & incline

---
+++ dynamics/pulleys.js
@@ -0,0 +1,78 @@
+g = 0.5;
+m1 = 10;
+m2 = 20;
+v = 0;
+x = 0;
+
+let m1s;
+let m2s;
+
+function setup() {
+  cnvs = createCanvas(400, 600);
+  cnvs.parent('cnvs');
+  m1s = createSlider(1, 5, 2.5, 0.1);
+  m2s = createSlider(1, 5, 2.5, 0.1);
+
+  m1s.position(cnvs.position().x+10, cnvs.position().y+10);
+  m2s.position(cnvs.position().x+10, cnvs.position().y+70);
+
+  m1s.style('width', '80px');
+  m2s.style('width', '80px');
+}
+
+function draw() {
+  m1 = m1s.value();
+  m2 = m2s.value();
+
+
+  background(255);
+
+  v += getAccel();
+
+  if (abs(x) > 230) {
+    v = 0;
+    if (x > 0)
+      x = 230;
+    else
+      x = -230;
+  }
+
+  x += v;
+
+  fill(200);
+  strokeWeight(5);
+  ellipse(200,50, 150, 50);
+
+  fill(0);
+
+  text("m1 = " + m1 + " kg", 15, 50);
+  text("m2 = " + m2 + " kg", 15, 110);
+
+  push();
+  translate(115, 300+x);
+  rotate(-PI/2);
+  var KE = round(0.5*m1*v*v);
+  var PE = round(m1*g*(230-x));
+  if (abs(x) >= 230) {
+    KE = 0;
+  }
+  if (x >= 230)
+    PE = 0;
+  text("KE = " + KE + " J", -65, 50);
+  text("PE = " + PE + " J", -70, 80);
+  pop();
+
+  line(125, 50, 125, x+320);
+  line(275, 50, 275, 320-x);
+
+  strokeWeight(1);
+
+  fill(50);
+  rect(100, 320+x, 50);
+  rect(250, 320-x, 50);
+
+}
+
+function getAccel() {
+  return (m1 * g - m2 * g) / (m1 + m2);
+}
