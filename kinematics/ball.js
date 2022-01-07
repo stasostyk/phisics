a769aee3fc1b88030ce0a54ea4f8d04d68d6cd94
stashito <s.ostyks@gmail.com>
2022-01-07 16:08:51 +0300

    euler integration

--- kinematics/ball.js
+++ kinematics/ball.js
@@ -12,26 +12,35 @@ class Ball {
   }
 
   update() {
-    this.x+=this.vx;
-    this.y+=this.vy;
-
-    this.vy += this.g;
-
-    if (this.x-5 <= 0 || this.x+5 >= width) {
-      this.vx *= -1;
-    }
-    if (this.y-5 <= 0) {
-      this.vy *= -1;
-    }
-
-    if (this.y+5 >= 400) {
-      this.y = 400-5;
-      this.vy *= -0.7;
-
-      if (this.vy < 0 && this.vy > -this.g*5) {
-        this.vy = 0;
-        this.vx = 0;
+// euler integration with timestep dt=1000
+    for (let i = 0; i < 1000; i++) {
+      // bounce detection
+      let collided_x = false;
+      let collided_y = false;
+      if (this.x-5 <= 0 || this.x+5 >= width) {
+        this.vx *= -1;
+        collided_x = true;
       }
+      if (this.y+5 >= 400 || this.y-5 <= 0) {
+        this.vy *= -1;
+        collided_y = true;
+
+        // rolling friction
+        if (this.vy < 1 && this.vy > -1)
+          this.vx*=0.99999;
+      }
+
+      // update rates of change
+      this.x+=this.vx/1000;
+      this.vy += this.g/1000;
+      this.y+=this.vy/1000;
+
+      // account for loss in energy
+      if (collided_x)
+        this.vx *= 0.7;
+
+      if (collided_y)
+        this.vy *= 0.7;
     }
   }
 }
