85486569f4dca22c28e42183158dc9a698a022b6
stashito <s.ostyks@gmail.com>
2022-01-26 17:08:51 +0300

    albedo simulation

---
+++ climate/albedo.js
@@ -0,0 +1,106 @@
+let photons = [new Ball(100, 100, 'green')];
+let t = 0;
+let temp = 1;
+let biome;
+let power;
+let total = 0;
+
+function setup() {
+  cnvs = createCanvas(800, 500);
+  cnvs.parent('cnv');
+  noStroke();
+
+  biome = createSelect();
+  biome.position(cnvs.position().x+720, cnvs.position().y+25);
+  biome.option("snow");
+  biome.option("grass");
+  biome.option("ocean");
+  biome.option("desert");
+  biome.changed(changeBiome);
+
+  power = createSlider(1, 101, 21, 10);
+  power.position(cnvs.position().x+720, cnvs.position().y+55);
+  power.style('width', '65px');
+}
+
+
+function draw() {
+  background(153, 196, 250);
+
+  for (let i = 0; i < photons.length; i++)
+    photons[i].draw();
+
+  fill(245, 178, 30);
+  circle(100, 100, 75 + abs(((t)%60)-30)/20);
+
+  switch (biome.value()) {
+    case "ocean":
+      fill(0, 0, 200);
+      break;
+    case "desert":
+      fill(241, 190, 108);
+      break;
+    case "snow":
+      fill(224, 255, 255)
+      break;
+    default:
+      fill(0, 200, 0);
+  }
+  rect(0, 450, 800, 50);
+
+
+  fill(250);
+  rect(18, 402, 14, -104)
+  fill(200,0,0);
+  rect(20, 400, 10, -temp);
+  if (temp > 100)
+    temp = 100;
+
+  fill(0);
+  if (biome.value() == "ocean")
+    fill(255);
+  textSize(30);
+  text("" + (temp-1) + "/" + total + " photons absorbed", 240, 485);
+
+  textSize(15);
+  fill(0);
+  text("Biome: ", 670, 40);
+  text("Sun's Power: ", 629, 70);
+
+  push();
+  rotate(3*PI / 2.0);
+  text("Temperature", -395, 50);
+  pop();
+
+  update();
+}
+
+function update() {
+  t++;
+
+  for (let i = 0; i < photons.length; i++) {
+    if (photons[i].update(biome.value()))
+      temp++;
+    if (photons[i].y < -5)
+      photons.splice(i, 1);
+  }
+
+  if (t % (Math.floor(101/power.value())*4) == 0) {
+    let x = 80 + Math.floor((Math.random() * 40) + 1);
+    let y = 80 + Math.floor((Math.random() * 40) + 1);
+    clr = Math.floor((Math.random() * 3));
+    if (clr == 0)
+      clr = 'green';
+    else if (clr == 1)
+      clr = 'blue';
+    else
+      clr = 'red';
+    photons.push(new Ball(x, y, clr));
+    total++;
+  }
+}
+
+function changeBiome() {
+  temp = 1;
+  total = 0;
+}
---
+++ climate/ball.js
@@ -0,0 +1,52 @@
+class Ball {
+  constructor(x, y, clr) {
+    this.x = x;
+    this.y = y;
+    var angle = 0.9+(Math.random()/10);
+    this.vx = Math.cos(angle);
+    this.vy = Math.sin(angle);
+
+    this.name = clr;
+    this.r = Math.floor(Math.random()*150);
+    this.g = Math.floor(Math.random()*150);
+    this.b = Math.floor(Math.random()*150);
+    if (clr == "red")
+      this.r = 255;
+    else if (clr == "green")
+      this.g = 255;
+    else
+      this.b = 255;
+  }
+
+  update(biome) {
+    this.x += this.vx*this.y/150;
+    this.y += this.vy*this.y/150;
+
+    if (this.y >= 445) {
+      let probability = 0;
+      if (biome == "grass") {
+        if (this.name == "green")
+          probability = 50;
+      } else if (biome == "ocean") {
+        if (this.name == "blue")
+          probability = 50;
+      } else if (biome == "desert") {
+        if (this.name == "red" || this.name == "green")
+          probability = 50;
+      } else if (biome == "snow")
+        probability = 70;
+      if (Math.floor(Math.random() * 100)+1 <= probability)
+        this.vy *= -1;
+      else {
+        this.y = -10;
+        return true
+      }
+    }
+    return false;
+  }
+
+  draw() {
+    fill(color(this.r, this.g, this.b));
+    circle(this.x, this.y, this.y/50);
+  }
+}
